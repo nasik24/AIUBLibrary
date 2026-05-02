950379208f0a90760a1b6397e7b095329fc34a1a
nasik24 <nasik24.bd@gmail.com>
2026-05-02 13:35:52 +0600

    Update Book.java

--- book/Book.java
+++ book/Book.java
@@ -2,13 +2,42 @@ package book;
 
 public class Book {
 
-    public int id;
-    public String title;
-    public String author;
+    
+    private int id;
+    private String title;
+    private String author;
 
+    
     public Book(int id, String title, String author) {
         this.id = id;
         this.title = title;
         this.author = author;
     }
+
+    // getter methods 
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    // setter methods 
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
 }
